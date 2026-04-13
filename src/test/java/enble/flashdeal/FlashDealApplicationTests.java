fe6fb4a3bc9a60af8ce5c119ca6a11eb576c5355
Enble <enble_777@naver.com>
2026-04-14 02:40:50 +0900

    chore: 프로젝트 초기 설정

---
+++ src/test/java/enble/flashdeal/FlashDealApplicationTests.java
@@ -0,0 +1,13 @@
+package enble.flashdeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashDealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
