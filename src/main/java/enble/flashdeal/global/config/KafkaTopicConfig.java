39af8c7f8e50fe6b9a42fb5d6799d8ef88669a01
Enble <enble_777@naver.com>
2026-04-19 15:36:10 +0900

    chore: Kafka 의존성 추가 및 인프라 설정 (KRaft 단일 노드, 토픽 구성)

---
+++ src/main/java/enble/flashdeal/global/config/KafkaTopicConfig.java
@@ -0,0 +1,29 @@
+package enble.flashdeal.global.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    public static final String ORDER_CREATED = "order-created";
+    public static final String ORDER_CREATED_DLT = "order-created-dlt";
+
+    @Bean
+    public NewTopic orderCreatedTopic() {
+        return TopicBuilder.name(ORDER_CREATED)
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic orderCreatedDltTopic() {
+        return TopicBuilder.name(ORDER_CREATED_DLT)
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+}
